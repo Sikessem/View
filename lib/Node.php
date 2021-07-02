99d785d8a66708c59d24bd0fd546d9300fbd595d
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-07-03 01:58:26 +0200

    Create the HTML node class

---
+++ lib/Node.php
@@ -0,0 +1,20 @@
+<?php namespace Ends\Html;
+/**
+ * The HTML node class
+ *
+ * @author SIGUI Kessé Emmanuel
+ * @package sikessem/html
+ * @license Apache-2.0
+ */
+abstract class Node {
+  /**
+   * Get the node render
+   *
+   * @return string The node render
+   */
+  abstract public function render(): string;
+
+  public function __toString(): string {
+    return $this->render();
+  }
+}
